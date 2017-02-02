41a063e8d86e6c651f8bb2fea8bde1566f095098
mjohn365 <mjohn365@uncc.edu>
2017-02-02 12:33:58 -0500

    Added some basic I/O to the program.

--- src/sampleProgs.d
+++ src/sampleProgs.d
@@ -1,14 +1,27 @@
 import std.stdio;
+//Allows the use of string methods like strip()
+import std.string;
 
 void main() {
-	//Comment this out once we have meaningful code.
-	writeln("Code not started.");
 
 	//Part 1: Major Data Structures
 
 
 	//Part 2: I/O
 
+	//An array of String type
+	char[] state;
+	write("How are you doing today?: ");
+	readln(state);
+	//Removes control characters (space, enter, etc)
+	state = strip(state);
+	write("You said you are doing ", state, "!");
+
+	int days;
+	writeln("\nHow many days until this assignment is due?: ");
+	//The space before %s prevents the enter key from holding up output
+	readf(" %s", &days);
+	writeln("There are ", days, " days left for this assignment.");
 
 	//Part 3: Common Control Structures
 }
