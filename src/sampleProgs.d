71c6a1a96d80bc8779c42520ad55d1e72083e032
mjohn365 <mjohn365@uncc.edu>
2017-02-06 21:27:48 -0500

    Put David's movie structure in our sampleProgs.d and recompiled.

--- src/sampleProgs.d
+++ src/sampleProgs.d
@@ -2,11 +2,47 @@ import std.stdio;
 //Allows the use of string methods like strip()
 import std.string;
 
+struct MovieData
+{
+	char[] title;
+	char[] director;
+	uint yearReleased;
+	uint duration;
+};
+
+void displayMovie(MovieData movie)
+{
+	writeln("\n", movie.title, "\nDirected By: ", movie.director);
+	writeln("Released: ", movie.yearReleased, "\n", movie.duration, " minutes.");
+}
+
 void main() {
 
 	
 
 	//Part 1: Major Data Structures
+
+	//Simple Data Structure
+	MovieData movie1;
+	MovieData movie2;
+
+	//movie1 info
+	//.dup creates a dynamic array of the same size and copies the contents of the array into it. 
+	movie1.title	     = "Interstellar".dup;
+	movie1.director 	 = "Christopher Nolan".dup;
+	movie1.yearReleased  = 2014;
+	movie1.duration 	 = 169;
+
+	//movie2 info
+	movie2.title         = "2001: A Space Odyssey".dup;
+	movie2.director      = "Stanley Kubrick".dup;
+	movie2.yearReleased  = 1968;
+	movie2.duration      = 142;
+
+
+	displayMovie(movie1);
+	displayMovie(movie2);
+	writeln("---------------------");
 
 	//Example of an associative array
 	int[string] aaTest ; //array of integers with string keys
